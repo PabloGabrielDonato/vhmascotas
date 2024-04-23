82c348b9fb9723791c24d3e4b7e64214664aef94
PabloGabrielDonato <iphone.pablo2002@gmail.com>
2024-04-22 22:26:18 -0300

    Se corrigieron errores ortograficos en la seccion de graficos-7dashboard

--- app/Filament/Widgets/AnimalesJardin.php
+++ app/Filament/Widgets/AnimalesJardin.php
@@ -12,21 +12,21 @@ class AnimalesJardin extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make(label:'Total Aniamales en jardincito', value: Jardincito::count())
+            Stat::make(label:'Total Animales en jardincito', value: Jardincito::count())
 
             ->description('Cantidad de animales registrados en el jardincito')
             ->descriptionIcon('heroicon-m-arrow-trending-up')
             ->color('success')
             ->chart([7,3,4,5,6,8,18,29]),
             
-            Stat::make(label:'Total Aniamales en jardincito de los lunes', value: Jardincito::where('dia', 'lunes')->count())
+            Stat::make(label:'Total Animales en jardincito de los lunes', value: Jardincito::where('dia', 'lunes')->count())
 
-            ->description('Cantidad de animales registrados en el jardincito de los lunes')
+            ->description('Cantidad de Animales registrados en el jardincito de los lunes')
             ->descriptionIcon('heroicon-m-arrow-trending-up')
             ->color('success')
             ->chart([7,3,4,5,6,8,18,29]),
             
-            Stat::make(label:'Total Aniamales en jardincito los miércoles', value: Jardincito::where('dia', 'lunes')->count())
+            Stat::make(label:'Total Animales en jardincito los miércoles', value: Jardincito::where('dia', 'lunes')->count())
 
             ->description('Cantidad de animales registrados en el jardincito de los miércoles')
             ->descriptionIcon('heroicon-m-arrow-trending-up')
